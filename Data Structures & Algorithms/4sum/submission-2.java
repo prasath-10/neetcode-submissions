0d3efffd7667dc6ab5ba4d90179122b1eb4506de
Prasath <prasaths1007@gmail.com>
2026-09-04 11:57:24 +0530

    Add: 4sum - submission-2

---
+++ Data Structures & Algorithms/4sum/submission-2.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>>ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0 ; i < nums.length ; i++)
+        {
+            if(i >0 && nums[i] == nums[i -1]){
+                continue;
+            }
+            for(int  j = i + 1 ; j < nums.length ; j++)
+            {
+                if(j > i +1 && nums[j] == nums[j -1]){
+                    continue;
+                }
+                int left = j + 1;
+                int right  = nums.length - 1;
+                while(left < right){
+                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+                    if(sum == target){
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[left]);
+                        temp.add(nums[right]);
+                        ans.add(temp);
+                        while(left < right && nums[left] == nums[left +  1]){
+                            left++;
+                        }
+                        while(left < right && nums[right] == nums[right - 1]){
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    }
+                    else if(sum > target){
+                        right--;
+                    }
+                    else{
+                        left++;
+                    }
+                }
+            }
+        }
+        return ans;
+        
+    }
+}
